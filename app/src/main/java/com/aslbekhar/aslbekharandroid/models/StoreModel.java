eff7abf8166be761fb3e01df8bdb19bd56c33965
Amin <aminkeshavarzian@gamil.com>
2016-06-24 20:36:32 +0430

    Phase 1 beta 2 storemodel unfinished

--- app/src/main/java/com/aslbekhar/aslbekharandroid/models/StoreModel.java
+++ app/src/main/java/com/aslbekhar/aslbekharandroid/models/StoreModel.java
@@ -42,6 +42,13 @@ public class StoreModel {
     int sDiscount;
     String sVerified;
     String bCategoryId;
+    String dStartDate;
+    String dEndDate;
+    String dStartDateFa;
+    String dEndDateFa;
+    String dPrecentage = "0";
+    String dNote;
+    String distance;
 
     public StoreModel() {
     }
@@ -200,6 +207,70 @@ public class StoreModel {
 
     public void setbCategoryId(String bCategoryId) {
         this.bCategoryId = bCategoryId;
+    }
+
+    public String getsHours() {
+        return sHours;
+    }
+
+    public void setsHours(String sHours) {
+        this.sHours = sHours;
+    }
+
+    public String getdStartDate() {
+        return dStartDate;
+    }
+
+    public void setdStartDate(String dStartDate) {
+        this.dStartDate = dStartDate;
+    }
+
+    public String getdEndDate() {
+        return dEndDate;
+    }
+
+    public void setdEndDate(String dEndDate) {
+        this.dEndDate = dEndDate;
+    }
+
+    public String getdStartDateFa() {
+        return dStartDateFa;
+    }
+
+    public void setdStartDateFa(String dStartDateFa) {
+        this.dStartDateFa = dStartDateFa;
+    }
+
+    public String getdEndDateFa() {
+        return dEndDateFa;
+    }
+
+    public void setdEndDateFa(String dEndDateFa) {
+        this.dEndDateFa = dEndDateFa;
+    }
+
+    public String getdPrecentage() {
+        return dPrecentage;
+    }
+
+    public void setdPrecentage(String dPrecentage) {
+        this.dPrecentage = dPrecentage;
+    }
+
+    public String getdNote() {
+        return dNote;
+    }
+
+    public void setdNote(String dNote) {
+        this.dNote = dNote;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
     }
 
     public static void getStoreListFromAssets() {
