c89445d9e984509b483b13fdbe8c94d66dfe0167
Amin <aminkeshavarzian@gamil.com>
2016-08-19 13:14:47 +0430

    my store

---
+++ app/src/main/java/com/aslbekhar/aslbekharandroid/activities/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.aslbekhar.aslbekharandroid.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aslbekhar.aslbekharandroid.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registeration);
+    }
+}
